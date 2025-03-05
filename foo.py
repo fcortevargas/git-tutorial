ece34ea98e7e13923d1c7feefb4dca812487ffc5
Fernando Corte Vargas <fernando.cortevargas@ticketswap.com>
2025-03-05 11:34:22 +0100

    ✨ Add main function to execute hello_world and goodbye_world

--- foo.py
+++ foo.py
@@ -1,5 +1,15 @@
 def hello_world():
     return "Hello, World!"
 
+
 def goodbye_world():
     return "Goodbye, World!"
+
+
+def main():
+    print(hello_world())
+    print(goodbye_world())
+
+
+if __name__ == "__main__":
+    main()
